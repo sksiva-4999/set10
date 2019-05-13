ba17f4d087b5d6af94fba20879916b97efdb4f4c
sksiva-4999 <49307482+sksiva-4999@users.noreply.github.com>
2019-05-13 15:38:03 +0530

    99.java

---
+++ 99.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sum
+{
+	
+	public static void main(String[] args) 
+	{
+		int a,b,c;
+        Scanner	sj = new Scanner(System.in);
+        a=sj.nextInt();
+        b=sj.nextInt();
+        c=sj.nextInt();
+      
+        System.out.print((a*b)%c);
+	}
+}
